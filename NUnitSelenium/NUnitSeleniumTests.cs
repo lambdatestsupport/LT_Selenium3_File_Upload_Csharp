43e39d7d41b6129ac54860da0522a7a7eaf530ff
ritamganguli <35348707+ritamganguli@users.noreply.github.com>
2023-10-08 20:02:31 +0530

    commit

---
+++ NUnitSelenium/NUnitSeleniumTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace NUnitSelenium
+{
+    [TestFixture("chrome", "latest", "Windows 10")]
+    [Parallelizable(ParallelScope.Children)]
+    public class NUnitSeleniumSample
+    {
+        public static string LT_USERNAME ="shubhamr";
+        public static string LT_ACCESS_KEY ="dl8Y8as59i1YyGZZUeLF897aCFvIDmaKkUU1e6RgBmlgMLIIhh";
+        public static string seleniumUri = "https://hub.lambdatest.com:443/wd/hub";
+
+
+        ThreadLocal<IWebDriver> driver = new ThreadLocal<IWebDriver>();
+        private String browser;
+        private String version;
+        private String os;
+        private string[] ltFile;
+
+        public object callStack { get; private set; }
+
+        public NUnitSeleniumSample(String browser, String version, String os)
+        {
+            this.browser = browser;
+            this.version = version;
+            this.os = os;
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.SetCapability(CapabilityType.BrowserName, browser);
+            capabilities.SetCapability(CapabilityType.Version, version);
+            capabilities.SetCapability(CapabilityType.Platform, os);
+            capabilities.SetCapability("testName", "Ritam");
+            string[] ltFile = new string[] { "dotnet-install.sh" };
+            capabilities.SetCapability("lambda:userFiles", ltFile);
+            capabilities.SetCapability("user", LT_USERNAME);
+            capabilities.SetCapability("accessKey", LT_ACCESS_KEY);
+
+            capabilities.SetCapability("name",
+            String.Format("{0}:{1}",
+            TestContext.CurrentContext.Test.ClassName,
+            TestContext.CurrentContext.Test.MethodName));
+            driver.Value = new RemoteWebDriver(new Uri(seleniumUri), capabilities, TimeSpan.FromSeconds(600));
+            Console.Out.WriteLine(driver);
+        }
+
+        [Test]
+       public void Todotest()
+        {
+            {
+                Console.WriteLine("Navigating to todos app.");
+                driver.Value.Navigate().GoToUrl("C:\\Users\\ltuser\\Downloads");
+                Thread.Sleep(7000);
+                driver.Value.Navigate().GoToUrl("https://the-internet.herokuapp.com/upload");
+                Thread.Sleep(9000);
+                driver.Value.FindElement(By.Id("file-upload")).SendKeys("C:\\Users\\ltuser\\Downloads\\dotnet-install.sh");
+                // Thread.Sleep(5000);
+                driver.Value.FindElement(By.Id("file-submit")).Click();
+
+            }
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            bool passed = TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Passed;
+            try
+            {
+                // Logs the result to LambdaTest
+                ((IJavaScriptExecutor)driver.Value).ExecuteScript("lambda-status=" + (passed ? "passed" : "failed"));
+            }
+            finally
+            {
+                
+                // Terminates the remote webdriver session
+                driver.Value.Quit();
+            }
+        }
+    }
+}
